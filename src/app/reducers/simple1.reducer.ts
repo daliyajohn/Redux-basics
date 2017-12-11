063c5f24c72da9255af529dfc81348ac8ffa7604
daliyajohn <daliyajohn75@gmail.com>
2017-12-11 16:01:34 +0530

    UI fix

--- src/app/reducers/simple1.reducer.ts
+++ src/app/reducers/simple1.reducer.ts
@@ -2,11 +2,12 @@ import { Store } from '@ngrx/store';
 import { Action } from '@ngrx/store';
 
 export function simpleReducer1 (state: string= 'hello', action: Action ) {
-    console.log(action.type, state);
     switch (action.type) {
         case 'SAVE':
+        console.log(action.type);
             return state = 'saved';
         case 'UPDATE':
+        console.log(action.type);
             return state = 'updated';
         default:
             return state = 'default';
